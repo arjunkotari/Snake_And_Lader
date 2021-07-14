eda2c59078df5402e69e9a75f22d2c01f89bc55c
arjunkotari <arjun.kotari123@gmail.com>
2021-07-14 17:27:34 +0530

    Added:Repeat the code till it reaches the position 100.

--- src/Snake_And_Lader/SnakeAndLader.java
+++ src/Snake_And_Lader/SnakeAndLader.java
@@ -13,23 +13,34 @@ public class SnakeAndLader {
 		/*
 		 * The Player rolls the die to get a number between 1 to 6.
 		 */
-		int rollDie = (int) (1 + Math.floor(Math.random() * 10) % 6);
-		System.out.println("Role die is: " + rollDie);
+
+		//System.out.println("Role die is: " + rollDie);
 		/*
 		 * checks for a Option. They are No Play,Ladder or Snake.	
-		 */
-		int checkOption= (int) (Math.floor(Math.random() * 10) % 3);
-		switch(checkOption) {
-		case noPlay:
-			System.out.println("No play. Player position is: "+player0);
-			break;
-		case lader:
-			player0 += rollDie;
-			System.out.println("Congrats you got lader. Position is: "+player0);
-			break;
-		default:
-			player0 -= rollDie;
-			System.out.println("Attecked by snake. Position is: "+player0);
-		}
-		}
+		 */		
+		while (player0 <=100)
+		{
+			int rollDie = (int) (1 + Math.floor(Math.random() * 10) % 6);
+			int checkOption= (int) (Math.floor(Math.random() * 10) % 3);
+			switch(checkOption) {		
+			case noPlay:
+				System.out.println("No play. Player position is: "+player0);
+				break;
+			case lader:
+				player0 += rollDie;
+				System.out.println("Congrats you got lader. Roll die is: "+rollDie +" Position is: "+player0);
+				break;
+			default:
+				player0 -= rollDie;
+				/*
+				 * player position moves below 0, then the player restarts from 0th position.
+				 */
+				if (player0 <= 0 )
+				{
+					player0 = 0;
+				}								
+				System.out.println("Attecked by snake. Roll die is: "+rollDie +" Position is: "+player0);
+				}
+		}			
+	}
 }
