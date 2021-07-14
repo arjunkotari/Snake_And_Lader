468017a08681d352394379c366855dbb45912688
arjunkotari <arjun.kotari123@gmail.com>
2021-07-14 16:21:44 +0530

    Added: Welcome message

---
+++ src/Snake_And_Lader/SnakeAndLader.java
@@ -0,0 +1,10 @@
+package Snake_And_Lader;
+
+public class SnakeAndLader {
+
+	public static void main(String[] args) {
+		//Welcome message.
+		System.out.println("Welcome to Snake and Lader simulator...");
+		}
+
+}
