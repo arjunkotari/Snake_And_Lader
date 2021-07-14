f1484dbe964a7e63f05bf996da4347ca5551addf
arjunkotari <arjun.kotari123@gmail.com>
2021-07-14 16:27:07 +0530

    Added: Single player at start position 0

--- src/Snake_And_Lader/SnakeAndLader.java
+++ src/Snake_And_Lader/SnakeAndLader.java
@@ -5,6 +5,8 @@ public class SnakeAndLader {
 	public static void main(String[] args) {
 		//Welcome message.
 		System.out.println("Welcome to Snake and Lader simulator...");
+		//game played with single player at start position 0.
+		int player0=0;
+		System.out.println("Starting position of player0 is: "+player0);
 		}
-
 }
