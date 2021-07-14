a0e3dd1f1010ca51f1392bec1a3d5e7b0a921c91
arjunkotari <arjun.kotari123@gmail.com>
2021-07-14 16:57:58 +0530

    checks for a option, that No play,Ladder or snake

--- src/Snake_And_Lader/SnakeAndLader.java
+++ src/Snake_And_Lader/SnakeAndLader.java
@@ -3,6 +3,8 @@ package Snake_And_Lader;
 public class SnakeAndLader {
 
 	public static void main(String[] args) {
+		final int noPlay = 0;
+		final int lader = 1;
 		//Welcome message.
 		System.out.println("Welcome to Snake and Lader simulator...");
 		//game played with single player at start position 0.
@@ -13,5 +15,21 @@ public class SnakeAndLader {
 		 */
 		int rollDie = (int) (1 + Math.floor(Math.random() * 10) % 6);
 		System.out.println("Role die is: " + rollDie);
+		/*
+		 * checks for a Option. They are No Play,Ladder or Snake.	
+		 */
+		int checkOption= (int) (Math.floor(Math.random() * 10) % 3);
+		switch(checkOption) {
+		case noPlay:
+			System.out.println("No play. Player position is: "+player0);
+			break;
+		case lader:
+			player0 += rollDie;
+			System.out.println("Congrats you got lader. Position is: "+player0);
+			break;
+		default:
+			player0 -= rollDie;
+			System.out.println("Attecked by snake. Position is: "+player0);
+		}
 		}
 }
